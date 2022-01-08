f5b760701ca3097449a07a3349e4ce35dfbf513c
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2022-01-07 22:55:03 -0500

    Se agrego otra manera distinta de tener texto enriquecido, esta vez se esta usando para el texto de la respuesta

--- web/src/components/Answer.js
+++ web/src/components/Answer.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export const Answer = ({ answer, userId, onDelete }) => (
   <aside className="answer">
-    <p className='respuesta'>{answer.answer}</p>
-    <button className={answer.userId === userId?"btn btn-danger delete1":"notShowDeleteButton" }onClick={()=>onDelete(answer)}>Eliminar Respuesta</button>
+    <div dangerouslySetInnerHTML={{ __html: answer.answer }} />{" "}
+    <button
+      className={
+        answer.userId === userId
+          ? "btn btn-danger delete1"
+          : "notShowDeleteButton"
+      }
+      onClick={() => onDelete(answer)}
+    >
+      Eliminar Respuesta
+    </button>
   </aside>
-)
+);
